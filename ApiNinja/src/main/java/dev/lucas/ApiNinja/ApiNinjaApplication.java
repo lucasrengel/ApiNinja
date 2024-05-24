600552cee8d48d6f88d411544d420f2f2c6ac560
lucas.rengel@estudante.sc.senai.br <lucaslangel@hotmail.com>
2024-05-24 14:26:53 -0300

    projeto criado

---
+++ ApiNinja/src/main/java/dev/lucas/ApiNinja/ApiNinjaApplication.java
@@ -0,0 +1,13 @@
+package dev.lucas.ApiNinja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNinjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiNinjaApplication.class, args);
+	}
+
+}
