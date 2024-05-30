18cfecc4cf980fc4b455d4e0ae1400bf27b69d46
lucasrengel <lucas_rengel@estudante.sc.senai.br>
2024-05-30 18:54:53 -0300

    add: ninjas service new method

--- ApiNinja/src/main/java/dev/lucas/ApiNinja/service/NinjasService.java
+++ ApiNinja/src/main/java/dev/lucas/ApiNinja/service/NinjasService.java
@@ -1,5 +1,6 @@
 package dev.lucas.ApiNinja.service;
 
+import dev.lucas.ApiNinja.model.Ninjas;
 import dev.lucas.ApiNinja.repository.NinjasRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,9 @@ public class NinjasService {
 
     @Autowired // instanciar a interface
     private NinjasRepository repo;
+
+    //Cria um novo ninja usando a instancia do NinjasRepository usando JPA
+    public Ninjas createNinja(Ninjas ninja){
+        return repo.save(ninja);
+    }
 }
