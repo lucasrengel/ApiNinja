7d0d8052f1b31b9569e131050b388c774b4f5752
lucasrengel <lucas_rengel@estudante.sc.senai.br>
2024-05-30 18:54:13 -0300

    update: ninjas repository import

--- ApiNinja/src/main/java/dev/lucas/ApiNinja/repository/NinjasRepository.java
+++ ApiNinja/src/main/java/dev/lucas/ApiNinja/repository/NinjasRepository.java
@@ -1,6 +1,6 @@
 package dev.lucas.ApiNinja.repository;
 
-import dev.lucas.ApiNinja.config.Ninjas;
+import dev.lucas.ApiNinja.model.Ninjas;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NinjasRepository extends JpaRepository<Ninjas, Long> {
