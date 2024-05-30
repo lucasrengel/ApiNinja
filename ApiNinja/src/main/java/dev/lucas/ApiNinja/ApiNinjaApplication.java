46f0141f48af62dd976bf9f0900bd3d9a5d3597b
lucasrengel <lucas_rengel@estudante.sc.senai.br>
2024-05-30 20:41:01 -0300

    update: formatting

--- ApiNinja/src/main/java/dev/lucas/ApiNinja/ApiNinjaApplication.java
+++ ApiNinja/src/main/java/dev/lucas/ApiNinja/ApiNinjaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiNinjaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiNinjaApplication.class, args);
+	public static void main(String[] args) { SpringApplication.run(ApiNinjaApplication.class, args);
+
 	}
 
 }
