475d19547f2d49205dbadd8eff0afdbfecb386ee
lucasrengel <lucas_rengel@estudante.sc.senai.br>
2024-05-30 20:40:09 -0300

    add: crud created

--- ApiNinja/src/main/java/dev/lucas/ApiNinja/service/NinjasService.java
+++ ApiNinja/src/main/java/dev/lucas/ApiNinja/service/NinjasService.java
@@ -5,14 +5,32 @@ import dev.lucas.ApiNinja.repository.NinjasRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class NinjasService {
 
-    @Autowired // instanciar a interface
-    private NinjasRepository repo;
+    @Autowired
+    private NinjasRepository repo; // instanciar a interface
 
-    //Cria um novo ninja usando a instancia do NinjasRepository usando JPA
+    //Cria um novo ninja e envia pro db usando a instancia do NinjasRepository usando JPA
     public Ninjas createNinja(Ninjas ninja){
         return repo.save(ninja);
+    }
+
+    //Faz uma consulta no db e devolve uma list de ninjas
+    public List<Ninjas> getAllNinjas(){
+        return repo.findAll();
+    }
+
+    //Faz uma consulta no db e retorna o ninja pelo id
+    public Optional<Ninjas> readNinja(Long id){
+        return repo.findById(id);
+    }
+
+    //Deletar um ninja pelo id
+    public void deleteNinja(Long id) {
+        repo.deleteById(id);
     }
 }
