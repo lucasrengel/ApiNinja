3da71b89ffc6b5e3406e34d2189189ede52e256d
lucasrengel <lucas_rengel@estudante.sc.senai.br>
2024-05-30 18:28:26 -0300

    update: JPA generic parameters

--- ApiNinja/src/main/java/dev/lucas/ApiNinja/repository/NinjasRepository.java
+++ ApiNinja/src/main/java/dev/lucas/ApiNinja/repository/NinjasRepository.java
@@ -1,8 +1,9 @@
 package dev.lucas.ApiNinja.repository;
 
+import dev.lucas.ApiNinja.config.Ninjas;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NinjasRepository extends JpaRepository {
+public interface NinjasRepository extends JpaRepository<Ninjas, Long> {
 
 
 }
